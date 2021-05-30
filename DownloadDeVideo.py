b11969015e8af14decd11957c09c475cc67d6f9f
josefim15 <josemessias9554@gmail.com>
2021-05-30 00:59:50 -0300

    Baixar videos

---
+++ DownloadDeVideo.py
@@ -0,0 +1,32 @@
+from pytube import YouTube
+from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+def baixar_video():
+    
+    #COLANDO O LINK DO VÍDEO
+    
+    url = file.lineEdit.text()
+    diretorio = file.lineEdit_2.text()
+    tube = YouTube(url)
+    #DOWNLOAD DO VÍDEO
+    
+    video = tube.streams.get_highest_resolution()
+    video.download(diretorio)
+    
+    #CAIXA DE MENSSAGEM
+    
+    msg = QMessageBox()
+    msg.setWindowTitle('DOWNLOAD')
+    msg.setText('Download concluído')
+    msg.setIcon(QMessageBox.Information)
+    
+    x = msg.exec_()
+    print('OK')
+    
+app=QtWidgets.QApplication([])
+file=uic.loadUi('C:/Users/José Messias/Desktop/PYQT5/Baixar Videos/baixar.ui')
+file.pushButton.clicked.connect(baixar_video)
+
+file.show()
+app.exec()
